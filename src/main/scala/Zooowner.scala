3c4227844e9a8903f37d28d690ad790e82850641
Anton Karamanov <ataraxer@gmail.com>
2014-04-27 23:56:46 +0400

    Add `create` method

--- src/main/scala/Zooowner.scala
+++ src/main/scala/Zooowner.scala
@@ -5,6 +5,8 @@ import scala.concurrent.duration._
 import org.apache.zookeeper.ZooKeeper
 import org.apache.zookeeper.ZooKeeper.States
 import org.apache.zookeeper.Watcher.Event.KeeperState._
+import org.apache.zookeeper.CreateMode._
+import org.apache.zookeeper.KeeperException
 
 
 object Zooowner {
@@ -18,6 +20,10 @@ case class Zooowner(servers: String,
                    (onConnection: Zooowner => Unit)
 {
   private var client: ZooKeeper = null
+
+  // path prefix should be simple identifier
+  if (pathPrefix contains "/")
+    throw new IllegalArgumentException
 
   val watcher = Watcher {
     case SyncConnected => {
@@ -41,6 +47,27 @@ case class Zooowner(servers: String,
   def isConnected = client.getState == States.CONNECTED
 
   connect()
+
+  def create(path: String, data: String,
+             persisten: Boolean = false,
+             sequential: Boolean = false) =
+  {
+    val absolutePath = pathPrefix + path
+
+    val createMode = (persisten, sequential) match {
+      case (true, true)   => PERSISTENT_SEQUENTIAL
+      case (true, false)  => PERSISTENT
+      case (false, true)  => EPHEMERAL_SEQUENTIAL
+      case (false, false) => EPHEMERAL
+    }
+
+    try {
+      client.create(absolutePath, data.getBytes, null, createMode)
+    } catch {
+      case _: KeeperException => ???
+      case _: InterruptedException => ???
+    }
+  }
 }
 
 
--- src/test/scala/Zooowner.scala
+++ src/test/scala/Zooowner.scala
@@ -12,10 +12,30 @@ class ZooownerSpec extends UnitSpec {
     var timeout = 0
 
     var zk: Zooowner =
-      Zooowner("localhost:9181", 15.seconds, "/prefix") {
+      Zooowner("localhost:9181", 15.seconds, "prefix") {
         client => {
           connected = true
           client.isConnected should be (true)
+        }
+      }
+
+    while (!connected) {
+      timeout += 1
+      if (timeout > 3) fail
+      Thread.sleep(1000)
+    }
+  }
+
+
+  it should "create ephemeral nodes by default" in {
+    var connected = false
+    var timeout = 0
+
+    var zk: Zooowner =
+      Zooowner("localhost:9181", 15.seconds, "prefix") {
+        client => {
+          connected = true
+          client.create("/test", "value")
         }
       }
 
