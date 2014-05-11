635cd6263d07ca551134b8bc8c49932b3f73aad3
Anton Karamanov <ataraxer@gmail.com>
2014-05-11 14:32:55 +0400

    Split Watcher into StateWatcher and EventWatcher

--- src/main/scala/Watcher.scala
+++ src/main/scala/Watcher.scala
@@ -1,23 +1,40 @@
 package com.ataraxer.zooowner
 
-import org.apache.zookeeper.{Watcher => ZooWatcher, WatchedEvent}
-import org.apache.zookeeper.Watcher.Event.KeeperState
+import org.apache.zookeeper.{Watcher => ZKWatcher, WatchedEvent}
+import org.apache.zookeeper.Watcher.{Event => ZKEvent}
+import org.apache.zookeeper.Watcher.Event.{KeeperState, EventType}
 
 
 object Watcher {
-  type Reaction = PartialFunction[KeeperState, Unit]
+  type Reaction[T] = PartialFunction[T, Unit]
 
-  val default: Reaction = { case _ => }
+  def default[T]: Reaction[T] = { case _ => }
 }
 
 
-case class Watcher(reaction: Watcher.Reaction) extends ZooWatcher {
+sealed abstract class Watcher[T]
+  (reaction: Watcher.Reaction[T])
+    extends ZKWatcher
+{
   import Watcher._
 
   def process(event: WatchedEvent) = {
-    (reaction orElse default)(event.getState)
+    (reaction orElse default[T]) {
+      extract(event)
+    }
   }
+
+  def extract(event: WatchedEvent): T
 }
+
+
+case class StateWatcher(reaction: Watcher.Reaction[KeeperState])
+    extends Watcher[KeeperState](reaction)
+{ def extract(event: WatchedEvent) = event.getState }
+
+case class EventWatcher(reaction: Watcher.Reaction[EventType])
+    extends Watcher[EventType](reaction)
+{ def extract(event: WatchedEvent) = event.getType }
 
 
 // vim: set ts=2 sw=2 et:
