863e7ac49344cb5594b45f225726ccb934636462
Anton Karamanov <ataraxer@gmail.com>
2014-06-25 23:54:37 +0400

    Add publishing configuration

--- project/Build.scala
+++ project/Build.scala
@@ -38,6 +38,34 @@ object AkkaPattersBuild extends Build {
       "org.apache.curator" % "curator-test" % "2.4.2" % "test"
     ),
 
+    publishTo <<= version { (ver: String) =>
+      val nexus = "http://nexus.ataraxer.com/nexus/"
+      if (ver.trim.endsWith("SNAPSHOT")) {
+        Some("Snapshots" at nexus + "content/repositories/snapshots")
+      } else {
+        Some("Releases"  at nexus + "content/repositories/releases")
+      }
+    },
+
+    publishMavenStyle := true,
+
+    publishArtifact in Test := false,
+
+    pomIncludeRepository := { case _ => false },
+
+    pomExtra := (
+      <scm>
+        <url>git@github.com:ataraxer/zooowner.git</url>
+        <connection>scm:git:git@github.com:ataraxer/zooowner.git</connection>
+      </scm>
+      <developers>
+        <developer>
+          <id>ataraxer</id>
+          <name>Anton Karamanov</name>
+          <url>github.com/ataraxer</url>
+        </developer>
+      </developers>),
+
     parallelExecution := false
   )
 
@@ -46,7 +74,12 @@ object AkkaPattersBuild extends Build {
     ScoverageSbtPlugin.instrumentSettings ++
     Seq(
       name         := "zooowner",
-      version      := "0.2.1",
+      version      := "0.2.1-SNAPSHOT",
+      organization := "com.ataraxer",
+      homepage     := Some(url("http://github.com/ataraxer/zooowner")),
+      licenses     := Seq("MIT License" -> url(
+        "http://www.opensource.org/licenses/mit-license.php"
+      )),
       scalaVersion := "2.10.3"
     )
 
