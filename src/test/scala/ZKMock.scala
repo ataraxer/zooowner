5c7991990872d13c53eeb008650481d2b76da533
Anton Karamanov <ataraxer@gmail.com>
2014-07-09 22:34:34 +0200

    Add ZK Stat mocks

--- src/test/scala/ZKMock.scala
+++ src/test/scala/ZKMock.scala
@@ -2,6 +2,7 @@ package com.ataraxer.zooowner
 
 import org.apache.zookeeper.ZooKeeper
 import org.apache.zookeeper.ZooKeeper.States
+import org.apache.zookeeper.data.Stat
 
 import org.scalatest.Suite
 import org.scalatest.mock.MockitoSugar
@@ -13,6 +14,18 @@ import scala.concurrent.duration.FiniteDuration
 
 trait ZKMock extends MockitoSugar { this: Suite =>
   object zkMock {
+    val ephemeralStat = {
+      val stat = mock[Stat]
+      when(stat.getEphemeralOwner).thenReturn(1)
+      stat
+    }
+
+    val persistentStat = {
+      val stat = mock[Stat]
+      when(stat.getEphemeralOwner).thenReturn(0)
+      stat
+    }
+
     val client = {
       val zk = mock[ZooKeeper]
       when(zk.getState).thenReturn(States.CONNECTED)
