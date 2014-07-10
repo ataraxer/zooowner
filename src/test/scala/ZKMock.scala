6842049c560759c8114dbefc72a43352e294fdad
Anton Karamanov <ataraxer@gmail.com>
2014-07-10 23:59:09 +0200

    Extend default behaviour of ZK mock

--- src/test/scala/ZKMock.scala
+++ src/test/scala/ZKMock.scala
@@ -1,12 +1,15 @@
 package com.ataraxer.zooowner
 
-import org.apache.zookeeper.ZooKeeper
+import org.apache.zookeeper.{ZooKeeper, Watcher => ZKWatcher}
 import org.apache.zookeeper.ZooKeeper.States
+import org.apache.zookeeper.KeeperException._
 import org.apache.zookeeper.data.Stat
 
 import org.scalatest.Suite
 
 import org.mockito.Mockito._
+import org.mockito.Matchers._
+import org.mockito.Matchers.{eq => matchString}
 
 import scala.concurrent.duration.FiniteDuration
 
@@ -25,9 +28,18 @@ trait ZKMock {
       stat
     }
 
+
+    def anyWatcher = any(classOf[ZKWatcher])
+    def anyStat = any(classOf[Stat])
+
+
     val client = {
       val zk = mock(classOf[ZooKeeper])
       when(zk.getState).thenReturn(States.CONNECTED)
+      when(zk.getData(anyString, anyWatcher, anyStat))
+        .thenThrow(new NoNodeException)
+      when(zk.exists(anyString, anyWatcher))
+        .thenReturn(null)
       zk
     }
   }
