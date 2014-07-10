cda9cc9bbd377daa9982136cd6ca5387e460a5e6
Anton Karamanov <ataraxer@gmail.com>
2014-07-10 09:29:44 +0200

    Get rid of Mockito sugar

--- src/test/scala/ZKMock.scala
+++ src/test/scala/ZKMock.scala
@@ -5,29 +5,28 @@ import org.apache.zookeeper.ZooKeeper.States
 import org.apache.zookeeper.data.Stat
 
 import org.scalatest.Suite
-import org.scalatest.mock.MockitoSugar
 
 import org.mockito.Mockito._
 
 import scala.concurrent.duration.FiniteDuration
 
 
-trait ZKMock extends MockitoSugar { this: Suite =>
+trait ZKMock {
   object zkMock {
     val ephemeralStat = {
-      val stat = mock[Stat]
+      val stat = mock(classOf[Stat])
       when(stat.getEphemeralOwner).thenReturn(1)
       stat
     }
 
     val persistentStat = {
-      val stat = mock[Stat]
+      val stat = mock(classOf[Stat])
       when(stat.getEphemeralOwner).thenReturn(0)
       stat
     }
 
     val client = {
-      val zk = mock[ZooKeeper]
+      val zk = mock(classOf[ZooKeeper])
       when(zk.getState).thenReturn(States.CONNECTED)
       zk
     }
