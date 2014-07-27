9619c04213d3c953c6dbe9cbbcb2f6c8ff042756
Anton Karamanov <ataraxer@gmail.com>
2014-07-27 17:30:16 +0400

    Add data structure mirroring ZK node behaviour

---
+++ zooowner-common/src/main/scala/ZKNode.scala
@@ -0,0 +1,152 @@
+package com.ataraxer.zooowner.common
+
+import org.apache.zookeeper.KeeperException._
+import org.apache.zookeeper.data.Stat
+
+import scala.collection.mutable
+
+
+object Constants {
+  type ZKData = Option[Array[Byte]]
+  val AnyVersion = -1
+}
+
+import Constants._
+
+
+object ZKNode {
+  def apply(data: ZKData, persistent: Boolean = false): ZKNode = {
+    if (persistent) {
+      new PersistentNode(data)
+    } else {
+      new EphemeralNode(data)
+    }
+  }
+
+
+  def apply(data: String, persistent: Boolean): ZKNode = {
+    apply(Some(data.getBytes), persistent)
+  }
+
+
+  def apply(data: String): ZKNode = apply(data, false)
+  def apply(persistent: Boolean): ZKNode = apply(None, persistent)
+  def apply(): ZKNode = apply(None, false)
+}
+
+
+abstract class ZKNode(initialData: ZKData) {
+  protected object State {
+    var data = initialData
+    var version = 0
+    var children = mutable.Map.empty[String, ZKNode]
+  }
+
+
+  protected def checkVersion(version: Int) = {
+    val checkRequired = version != Constants.AnyVersion
+    if (checkRequired && version != State.version) {
+      throw new BadVersionException
+    }
+  }
+
+
+  protected def incrementVersion() = State.version += 1
+
+
+  // operations with node data itself
+  def data: ZKData = State.data
+
+  def data_=(data: ZKData) = {
+    set(data, version = Constants.AnyVersion)
+  }
+
+  def data_=(data: String) = {
+    set(Some(data.getBytes), version = Constants.AnyVersion)
+  }
+
+  def set(data: ZKData, version: Int): Unit = {
+    checkVersion(version)
+    incrementVersion()
+    State.data = data
+  }
+
+  def set(data: String, version: Int): Unit = {
+    set(Some(data.getBytes), version)
+  }
+
+  def children: Set[String] = State.children.keys.toSet
+
+  def child(name: String): ZKNode = {
+    State.children.get(name) match {
+      case Some(childNode) => childNode
+      case None => throw new NoNodeException
+    }
+  }
+
+  def version: Int = State.version
+  // generate Stat from State
+  def stat: Stat = {
+    val ephemeralOwner = if (persistent) 0 else 1
+    val dataLength = State.data.size
+    val numChildren = State.children.size
+    new Stat(
+      0, // czxid
+      0, // mzxid
+      0, // ctime
+      0, // mtime
+      State.version,
+      0, // cversion
+      0, // aversion
+      ephemeralOwner,
+      dataLength,
+      numChildren,
+      0) // pzxid
+  }
+
+  val persistent: Boolean
+  val sequential: Boolean = true
+
+  // operations with children
+  def create(
+    child: String, data: ZKData,
+    persistent: Boolean = true): ZKNode =
+  {
+    if (exists(child)) throw new NodeExistsException
+    val newNode = ZKNode(data, persistent)
+    State.children(child) = newNode
+    newNode
+  }
+
+  def create(child: String, data: String, persistent: Boolean): ZKNode = {
+    create(child, Some(data.getBytes), persistent)
+  }
+
+  def create(child: String, data: String): ZKNode = create(child, data, false)
+
+
+  def delete(child: String, version: Int = Constants.AnyVersion): Unit = {
+    val childNode = this.child(child)
+    childNode.checkVersion(version)
+    State.children -= child
+  }
+
+  def exists(child: String) = State.children.contains(child)
+}
+
+
+case class EphemeralNode(value: ZKData = None) extends ZKNode(value) {
+  val persistent = false
+
+  override def create(child: String, data: ZKData, persistent: Boolean) = {
+    throw new NoChildrenForEphemeralsException
+  }
+}
+
+
+case class PersistentNode(value: ZKData = None) extends ZKNode(value) {
+  val persistent = true
+}
+
+
+// vim: set ts=2 sw=2 et:
---
+++ zooowner-common/src/test/scala/ZKNode.scala
@@ -0,0 +1,108 @@
+package com.ataraxer.zooowner.common
+
+import org.apache.zookeeper.KeeperException._
+import com.ataraxer.test.UnitSpec
+import scala.language.implicitConversions
+import Constants.ZKData
+
+
+class NodeTreeSpec extends UnitSpec {
+
+  implicit class StringNode(node: ZKNode) {
+    def dataString = node.data.map(new String(_))
+  }
+
+
+  "ZKNode" should "consist be either persistent or epehemeral" in {
+    EphemeralNode(None) shouldBe a [ZKNode]
+    PersistentNode(None) shouldBe a [ZKNode]
+  }
+
+
+  it should "have data assigned to it" in {
+    ZKNode(None).data should be (None)
+    ZKNode("string").dataString should be (Some("string"))
+  }
+
+
+  it should "allow to change data" in {
+    val node = ZKNode("original-value")
+    node.data = "changed-value"
+    node.dataString should be (Some("changed-value"))
+  }
+
+
+  it should "store node's data version, starting from 0" in {
+    ZKNode("value").version should be (0)
+  }
+
+
+  it should "increment version on data change" in {
+    val node = ZKNode("original-value")
+    node.data = "changed-value"
+    node.version should be (1)
+  }
+
+
+  it should "check that correct version is being changed" in {
+    val node = ZKNode("original-value")
+    node.set("changed-value", version = 0)
+
+    intercept[BadVersionException] {
+      node.set("changed-value", version = 9000)
+    }
+  }
+
+
+  it should "allow to create children nodes under persistent once" in {
+    val node = ZKNode("original-value", persistent = true)
+    node.create("child", "child-value")
+
+    node.children should contain only ("child")
+    node.exists("child") should be (true)
+    node.child("child").dataString should be (Some("child-value"))
+  }
+
+
+  it should "not allow to create children under ephemeral once" in {
+    val node = ZKNode("original-value", persistent = false)
+
+    intercept[NoChildrenForEphemeralsException] {
+      node.create("child", "child-value")
+    }
+  }
+
+
+  it should "get child by name" in {
+    val node = ZKNode("original-value", persistent = true)
+    val child = node.create("child", "child-value")
+
+    node.child("child") should be (child)
+  }
+
+
+  it should "throw exception if requested child doesn't exists" in {
+    val node = ZKNode("original-value", persistent = true)
+
+    intercept[NoNodeException] {
+      node.child("child")
+    }
+  }
+
+
+  it should "allow to delete created children" in new {
+    val node = ZKNode("original-value", persistent = true)
+    node.create("child", "child-value")
+    node.delete("child")
+
+    node.children shouldBe empty
+    node.exists("child") should be (false)
+    intercept[NoNodeException] {
+      node.child("child")
+    }
+  }
+
+}
+
+
+// vim: set ts=2 sw=2 et:
