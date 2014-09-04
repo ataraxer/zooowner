08e3508694b17da0f9477c0c1b1760a3a3d75ffa
Anton Karamanov <ataraxer@gmail.com>
2014-09-04 23:35:09 +0400

    Add trait for custom zk data serialization

---
+++ zooowner-core/src/main/scala/ZKSerializer.scala
@@ -0,0 +1,41 @@
+package com.ataraxer.zooowner
+
+import scala.language.postfixOps
+
+
+object DefaultSerializers {
+  import ZKSerializer._
+
+  implicit val stringSerializer = {
+    new ZKSerializer[String] {
+      private val Encoding = "UTF-8"
+
+      def encode(string: String) = {
+        val wrappedString = Option(string)
+        wrappedString.map(_.getBytes(Encoding))
+      }
+
+      def decode(data: ZKData) = {
+        data.map {
+          new String(_, Encoding)
+        } orNull
+      }
+    }
+  }
+}
+
+
+object ZKSerializer {
+  type ZKData = Option[Array[Byte]]
+}
+
+
+trait ZKSerializer[T] {
+  import ZKSerializer._
+
+  def encode(value: T): ZKData
+  def decode(data: ZKData): Unit
+}
+
+
+// vim: set ts=2 sw=2 et:
