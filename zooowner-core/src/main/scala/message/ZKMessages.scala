e7d8907dbea321b26c20a708df840e638604c758
Anton Karamanov <ataraxer@gmail.com>
2015-04-04 17:55:31 +0300

    Revise messages type hierarchy

--- zooowner-core/src/main/scala/message/ZKMessages.scala
+++ zooowner-core/src/main/scala/message/ZKMessages.scala
@@ -6,52 +6,38 @@ import org.apache.zookeeper.KeeperException.Code
 
 
 sealed trait ZKMessage
-sealed trait ZKResponse
+
+sealed trait ZKRequest extends ZKMessage
+sealed trait ZKResponse extends ZKMessage
+
+sealed trait ZKSuccess extends ZKResponse
+sealed trait ZKFailure extends ZKResponse
+
 sealed trait ZKEvent extends ZKResponse
+
 sealed trait ConnectionEvent extends ZKEvent
 
 case object Connected extends ConnectionEvent
 case object Disconnected extends ConnectionEvent
 case object Expired extends ConnectionEvent
 
-case object ReadOnly extends ZKResponse
-case object BadVersion extends ZKResponse
+case object ReadOnly extends ZKFailure
+case object BadVersion extends ZKFailure
 
-case class NodeMeta(path: String, meta: ZKNodeMeta)
-  extends ZKMessage with ZKResponse
+case class NodeMeta(path: String, meta: ZKNodeMeta) extends ZKSuccess
+case class Node(node: ZKNode) extends ZKSuccess
+case class NodeChildren(path: String, children: List[String]) extends ZKSuccess
 
-case class Node(node: ZKNode)
-  extends ZKMessage with ZKEvent with ZKResponse
+case class NodeCreated(path: String, data: Option[ZKNode]) extends ZKEvent
+case class NodeChanged(path: String, data: Option[ZKNode]) extends ZKEvent
+case class NodeChildrenChanged(path: String, children: Seq[String]) extends ZKEvent
+case class NodeDeleted(path: String) extends ZKEvent
 
-case class NodeChildren(path: String, children: List[String])
-  extends ZKMessage with ZKEvent with ZKResponse
-
-case class NodeCreated(path: String, data: Option[ZKNode])
-  extends ZKMessage with ZKEvent with ZKResponse
-
-case class NodeChanged(path: String, data: Option[ZKNode])
-  extends ZKMessage with ZKEvent
-
-case class NodeChildrenChanged(path: String, children: Seq[String])
-  extends ZKMessage with ZKEvent
-
-case class NodeDeleted(path: String)
-  extends ZKMessage with ZKEvent with ZKResponse
-
-case class NoNode(path: String)
-  extends ZKMessage with ZKResponse
-
-case class NotEmpty(path: String)
-  extends ZKMessage with ZKResponse
-
-case class NodeExists(path: String)
-  extends ZKMessage with ZKResponse
-
-case class NodeIsEphemeral(path: String)
-  extends ZKMessage with ZKResponse
-
-case class Error(code: Code)
-  extends ZKMessage with ZKResponse
+case class NoNode(path: String) extends ZKFailure
+case class NotEmpty(path: String) extends ZKFailure
+case class NodeExists(path: String) extends ZKFailure
+case class NodeIsEphemeral(path: String) extends ZKFailure
+case class Error(code: Code) extends ZKFailure
 
 case class CreateNode(
     path: String,
@@ -66,18 +52,17 @@ case class DeleteNode(
     path: String,
     recursive: Boolean = false,
     version: Int = AnyVersion)
-  extends ZKMessage
+  extends ZKRequest
 
-case class SetNodeValue(path: String, data: String, version: Int = AnyVersion)
-  extends ZKMessage
+case class SetNodeValue(
+    path: String,
+    data: String,
+    version: Int = AnyVersion)
+  extends ZKRequest
 
-case class GetNodeValue(path: String)
-  extends ZKMessage
-
-case class GetNodeChildren(path: String)
-  extends ZKMessage
-
-case class WatchNode(path: String, persistent: Boolean = true)
+case class GetNodeValue(path: String) extends ZKRequest
+case class GetNodeChildren(path: String) extends ZKRequest
+case class WatchNode(path: String, persistent: Boolean = true) extends ZKRequest
 
 
 // vim: set ts=2 sw=2 et:
