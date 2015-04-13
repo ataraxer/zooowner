118a76ec3e615a6a9d84201255155d5d34beb885
Anton Karamanov <ataraxer@gmail.com>
2015-04-13 11:42:15 +0300

    ZKNode: add proxy methods for accessing metadata

--- zooowner-core/src/main/scala/ZKNode.scala
+++ zooowner-core/src/main/scala/ZKNode.scala
@@ -4,6 +4,14 @@ package zooowner
 case class ZKNode(path: ZKPath, data: ZKData, meta: ZKMeta) {
   def apply[T: ZKDecoder] = extract
   def extract[T: ZKDecoder] = implicitly[ZKDecoder[T]].decode(data)
+
+  def isPersistent = meta.ephemeral
+  def isEphemeral = !isPersistent
+
+  def creationTime = meta.creationTime
+  def modificationTime = meta.modificationTime
+  def version = meta.version
+  def childrenCount = meta.childrenCount
 }
 
 
